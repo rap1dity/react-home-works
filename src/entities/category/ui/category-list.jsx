edbc63d0203320d62c694bbbf46ba8da39544960
rap1dity <timeworstseal@gmail.com>
2025-03-28 17:20:26 +0200

    feat(L-2): Added category list

---
+++ src/entities/category/ui/category-list.jsx
@@ -0,0 +1,19 @@
+import * as styles from "./category-list.module.css";
+import { useState } from "react";
+
+export const CategoryList = ({ categories }) => {
+    const [activeCategory, setActiveCategory] = useState(0);
+
+    return (
+        <div className={styles.categoryContainer}>
+            {categories.map((category, index) => (
+                <span
+                    className={activeCategory === index ? styles.active : ''}
+                    onClick={() => setActiveCategory(index)}
+                >
+                    Desert
+                </span>
+            ))}
+        </div>
+    )
+}
