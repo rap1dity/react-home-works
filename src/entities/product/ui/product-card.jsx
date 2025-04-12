e0db8f523bfa356095d6aaadbf8a665696b51b5b
Andrew <Konwertik1@mail.ru>
2025-04-12 15:40:53 +0200

    feat(L-3): Menu page now loading meals from api

--- src/entities/product/ui/product-card.jsx
+++ src/entities/product/ui/product-card.jsx
@@ -1,30 +1,47 @@
-import * as styles from "@src/entities/product/ui/product-card.module.css";
+import * as styles from '@src/entities/product/ui/product-card.module.css';
+import React from 'react';
 
-export const ProductCard = ({ src, alt, title, price, description }) => {
+export class ProductCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      mealCount: 0,
+    }
+
+    this.handleInput = this.handleInput.bind(this);
+  }
+
+  handleInput(e) {
+    this.setState({ mealCount: e.target.value.replace(/\D/g, '') })
+  }
+
+  render() {
     return (
-        <div className={styles.product}>
-            <img src={src} alt={alt} />
-            <div className={styles.content}>
-                <div className={styles.contentHeader}>
-                    <span className={styles.contentHeaderTitle}>{title}</span>
-                    <span className={styles.contentHeaderPrice}>$ {price.toFixed(2)} USD</span>
-                </div>
-                <p>
-                    {description}
-                </p>
-                <div className={styles.contentControllers}>
-                    <input
-                        type='text'
-                        inputMode='numeric'
-                        defaultValue='0'
-                        pattern='[0-9]*'
-                        onInput={(e) => {
-                            e.target.value = e.target.value.replace(/\D/g, '')
-                        }}
-                    />
-                    <button>Add to cart</button>
-                </div>
+      <div className={styles.product}>
+        <img src={this.props.src} alt={this.props.alt}/>
+        <div className={styles.content}>
+          <div className={styles.generalInfo}>
+            <div className={styles.contentHeader}>
+              <span className={styles.contentHeaderTitle}>{this.props.title}</span>
+              <span className={styles.contentHeaderPrice}>$ {this.props.price.toFixed(2)} USD</span>
             </div>
+            <p>
+              {this.props.description}
+            </p>
+          </div>
+          <div className={styles.contentControllers}>
+            <input
+              type="text"
+              inputMode="numeric"
+              value={this.state.mealCount}
+              pattern="[0-9]*"
+              onInput={this.handleInput}
+            />
+            <button onClick={() => this.props.addToCart(this.state.mealCount)}>Add to cart</button>
+          </div>
         </div>
+      </div>
     )
+  }
 }
