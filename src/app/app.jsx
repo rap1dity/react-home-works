958490b2680def496281acb05fb317f03e4e7454
Andrew <Konwertik1@mail.ru>
2025-04-12 15:37:48 +0200

    feat(L-3): Added cartItemsCount to App state

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,17 +1,49 @@
-import { Header } from "@src/widgets/header";
+import { Header } from '@src/widgets/header';
 import { MenuPage } from '@src/pages/menu'
-import { useState } from "react";
+import React from 'react';
 import * as styles from './app.module.css';
-import { Footer } from "@src/widgets/footer/index.js";
+import { Footer } from '@src/widgets/footer/index.js';
+import { HomePage } from '@src/pages/home';
 
-export const App = () => {
-    const [activeTab, setActiveTab] = useState(0);
+export class App extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      activeTab: 0,
+      cartItemsCount: 0,
+    }
+
+    this.setActiveTab = this.setActiveTab.bind(this);
+    this.addToCart = this.addToCart.bind(this);
+  }
+
+  setActiveTab(newActiveTab) {
+    this.setState({
+      ...this.state,
+      activeTab: newActiveTab,
+    })
+  }
+
+  addToCart(count) {
+    this.setState({
+      ...this.state,
+      cartItemsCount: this.state.cartItemsCount + Number(count),
+    })
+  }
+
+  render() {
     return (
-        <div className={styles.container}>
-            <Header activeTab={activeTab} setActiveTab={setActiveTab} />
-            {activeTab === 1 && <MenuPage />}
-            <Footer />
-        </div>
+      <div className={styles.container}>
+        <Header
+          cartItemsCount={this.state.cartItemsCount}
+          activeTab={this.state.activeTab}
+          setActiveTab={this.setActiveTab}
+        />
+        {this.state.activeTab === 0 && <HomePage/>}
+        {this.state.activeTab === 1 && <MenuPage addToCart={this.addToCart}/>}
+        <Footer/>
+      </div>
     )
+  }
 }
