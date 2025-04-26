64158195c245414a5fe89df922b109112eef6168
Andrew <Konwertik1@mail.ru>
2025-04-26 16:03:24 +0200

    feat(L-6): Added error handling mechanism in apiFactory

--- src/shared/api/api-factory.js
+++ src/shared/api/api-factory.js
@@ -1,5 +1,9 @@
+import { throwApiError } from '@src/shared/api/throw-api-error.js';
+import { authApi } from '@src/shared/api/v1/web/auth';
+
 export class apiFactory {
   constructor(settings) {
+    this.baseUrl = settings?.baseUrl || import.meta.env.VITE_BACKEND_URL;
     this.urlPrefix = settings?.urlPrefix || '';
   }
 
@@ -32,11 +36,11 @@ export class apiFactory {
     return this.request('DELETE', url, config);
   }
 
-  async request(method, url, config = {}) {
+  async request(method, url, config = {}, isRetry) {
     const { headers, query, ...restConfig } = config;
 
     const searchParams = new URLSearchParams(query).toString();
-    const fullUrl = `${import.meta.env.VITE_MEAL_URL}${this.urlPrefix}${url}${searchParams ? `?${searchParams}` : ''}`;
+    const fullUrl = `${this.baseUrl}${this.urlPrefix}${url}${searchParams ? `?${searchParams}` : ''}`;
 
     console.log('%c[fetch request]', 'color: #4B71D6', method, fullUrl);
     if (headers) {
@@ -68,12 +72,26 @@ export class apiFactory {
 
     const statusText = `${response.status} ${response.statusText}`;
 
+    if (response.status === 401 && !isRetry) {
+      const success = await this.tryRefreshToken();
+
+      if (success) {
+        return this.request(method, url, config);
+      } else {
+        throw new Error('Unauthorized and refresh token failed');
+      }
+    }
+
     if (response.ok) {
       console.log('%c[fetch response]', 'color: #5BB557', statusText);
       console.log('data:', data);
     } else {
       console.error('%c[fetch response]', 'color #E84945', statusText);
       console.error('error:', data);
+      throwApiError({
+        data,
+        response,
+      });
     }
 
     this.saveToLocalStorage(fullUrl, data, response.status);
@@ -93,5 +111,39 @@ export class apiFactory {
     } catch (err) {
       console.error('Failed to save to localStorage', err);
     }
+  }
+
+  async tryRefreshToken() {
+    const refreshToken = localStorage.getItem('refreshToken');
+
+    if (!refreshToken) {
+      this.clearTokens();
+
+      return false;
+    }
+
+    try {
+      const { accessToken, refreshToken: newRefreshToken } = await authApi.refresh({ refreshToken });
+
+      if (accessToken && newRefreshToken) {
+        localStorage.setItem('accessToken', accessToken);
+
+        localStorage.setItem('refreshToken', newRefreshToken);
+
+        return true;
+      }
+    } catch (err) {
+      console.error('Failed to refresh token', err);
+    }
+
+    this.clearTokens();
+
+    return false;
+  }
+
+  clearTokens() {
+    localStorage.removeItem('accessToken');
+
+    localStorage.removeItem('refreshToken');
   }
 }
