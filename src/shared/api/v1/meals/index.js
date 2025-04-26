69b1767e38ea1de95d4e0423790e9abe6660fa4b
Andrew <Konwertik1@mail.ru>
2025-04-26 15:59:50 +0200

    refactor(L-6)

--- src/shared/api/v1/meals/index.js
+++ src/shared/api/v1/meals/index.js
@@ -1,7 +1,7 @@
-import { v1Api } from '@src/shared/api/v1/instance.js';
+import { v1MealApi } from '@src/shared/api/v1/instance.js';
 
 export const mealsApi = {
   async getMeals(query) {
-    return await v1Api.get('/meals', { query });
+    return await v1MealApi.get('/meals', { query });
   }
 }
