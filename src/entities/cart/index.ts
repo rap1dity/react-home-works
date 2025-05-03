ed4eda9a1ea91da9770ac03a306db8e496f741d2
Andrew <Konwertik1@mail.ru>
2025-05-03 13:50:34 +0200

    feat(L-9): Cart slice now contain full info about order, instead of count items in cart

--- src/entities/cart/index.ts
+++ src/entities/cart/index.ts
@@ -1,1 +1,4 @@
-export { addToCart, cartReducer } from './model/cart.slice.ts';
+export { addToCart, removeFromCart, resetCart, cartReducer } from './model/cart.slice.ts';
+export { selectCartMeals } from './model/selector.ts';
+export { OrderMealList } from './ui/order-meal-list.tsx';
+export { OrderForm } from './ui/order-form.tsx';
