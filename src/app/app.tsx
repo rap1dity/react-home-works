0ac31088bf4d259d1e3d90ae3367701c871c869b
Andrew <Konwertik1@mail.ru>
2025-05-03 13:48:54 +0200

    feat(L-9): Added routing for pages

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,21 +1,44 @@
 import { Header } from '@src/widgets/header';
 import { MenuPage } from '@src/pages/menu';
-import { useState } from 'react';
 import styles from './app.module.css';
 import { Footer } from '@src/widgets/footer';
 import { HomePage } from '@src/pages/home';
 import { LoginPage } from '@src/pages/login';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { setAuthorized } from '@src/entities/auth';
+import { RootState } from '@src/app/providers/store/config.ts';
+import { OrderPage } from '@src/pages/order';
 
 export const App = () => {
-  const [activeTab, setActiveTab] = useState(0);
+  const isAuthenticated = useSelector((store: RootState) => store.auth.isAuthorized);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem('accessToken');
+
+    if (accessToken) {
+      dispatch(setAuthorized());
+    }
+  }, [dispatch]);
 
   return (
-    <div className={styles.container}>
-      <Header activeTab={activeTab} setActiveTab={setActiveTab} />
-      {activeTab === 0 && <HomePage />}
-      {activeTab === 1 && <MenuPage />}
-      {activeTab === 3 && <LoginPage />}
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className={styles.container}>
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/menu" element={<MenuPage />} />
+          <Route path="/company" element={<div>Company Info</div>} />
+          {isAuthenticated ? (
+            <Route path="/order" element={<OrderPage />} />
+          ) : (
+            <Route path="/login" element={<LoginPage />} />
+          )}
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 };
